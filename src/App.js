59a847018ffd4526c41e3ec00bab49cf6c4a55f8
yiungyiung <yashparkhi.super@gmail.com>
2023-09-07 19:17:08 +0530

    created-repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Box} from '@mui/material';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="text-3xl font-bold italic underline"
+    height="100vh"
+    width="100vw"
+      sx={{
+        minHeight: "100vh",
+        overflowY: "auto",
+        scrollBehavior: "smooth",
+        position: "relative",
+        display:"flex",
+        flexDirection: "column",
+
+      }}
+    >
+      hello, world
+    </Box>
   );
 }
 
