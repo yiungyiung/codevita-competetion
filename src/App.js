829f2f825b312108cd67a00d237eab9e6a85022d
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2023-09-07 21:01:30 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import {Box} from '@mui/material';
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@mui/material";
+import Navbar from "./components/navbar";
 function App() {
   return (
-    <Box className="text-3xl font-bold italic underline"
-    height="100vh"
-    width="100vw"
-      sx={{
-        minHeight: "100vh",
-        overflowY: "auto",
-        scrollBehavior: "smooth",
-        position: "relative",
-        display:"flex",
-        flexDirection: "column",
-
-      }}
-    >
-      hello, world
-    </Box>
+    <div>
+      <Navbar />
+      <Box
+        className="text-3xl font-bold italic underline"
+        height="100vh"
+        width="100vw"
+        sx={{
+          minHeight: "100vh",
+          overflowY: "auto",
+          scrollBehavior: "smooth",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        hello, world
+      </Box>
+    </div>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+  Paper,
+  Grid,
+} from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+
+function Navbar() {
+  return (
+    <Paper elevation={3} sx={{ boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)" }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="sticky"
+          variant="outlined"
+          sx={{ backgroundColor: "#0066A1" }}
+        >
+          <Grid container alignItems="center">
+            <Grid item xs={6}>
+              <Toolbar>
+                <Stack direction="row" spacing={1}>
+                  <Button color="inherit" sx={{ fontSize: "15px" }}>
+                    HOME
+                  </Button>
+                  <Button color="inherit" sx={{ fontSize: "15px" }}>
+                    ABOUT US
+                  </Button>
+                  <Button color="inherit" sx={{ fontSize: "15px" }}>
+                    PROGRAM
+                  </Button>
+                  <Button color="inherit" sx={{ fontSize: "15px" }}>
+                    CONTACT
+                  </Button>
+                </Stack>
+              </Toolbar>
+            </Grid>
+            <Grid item xs={6} sx={{ textAlign: "right" }}>
+              <a target="_blank" rel="noopener noreferrer">
+                <InstagramIcon fontSize="large" sx={{ padding: "4px" }} />
+              </a>
+              <a target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon fontSize="large" sx={{ padding: "4px" }} />
+              </a>
+              <a target="_blank" rel="noopener noreferrer">
+                <TwitterIcon fontSize="large" sx={{ padding: "4px" }} />
+              </a>
+              <a target="_blank" rel="noopener noreferrer">
+                <EmailIcon fontSize="large" sx={{ padding: "4px" }} />
+              </a>
+            </Grid>
+          </Grid>
+        </AppBar>
+      </Box>
+    </Paper>
+  );
+}
+
+export default Navbar;
