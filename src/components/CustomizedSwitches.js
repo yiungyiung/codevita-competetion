9322f037164a85a4297a80289211f1357a9134b8
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2023-09-09 21:22:52 +0530

    switch partial work

--- src/components/CustomizedSwitches.js
+++ src/components/CustomizedSwitches.js
@@ -2,66 +2,72 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch, { SwitchProps } from "@mui/material/Switch";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
+import Switch from "@mui/material/Switch";
 
-const IOSSwitch = styled((props: SwitchProps) => (
-  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-))(({ theme }) => ({
-  width: 42,
-  height: 26,
-  padding: 0,
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 335,
+  height: 75,
+  padding: 7,
   "& .MuiSwitch-switchBase": {
+    margin: 1,
     padding: 0,
-    margin: 2,
-    transitionDuration: "300ms",
+    transform: "translateX(0px)",
     "&.Mui-checked": {
-      transform: "translateX(16px)",
       color: "#fff",
+      transform: "translateX(178px)",
+      "& .MuiSwitch-thumb:before": {
+        content: '"1"', // Change content to "1"
+        position: "absolute",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        left: 0,
+        top: 0,
+        color: "#fff",
+        fontSize: "24px", // Adjust font size as needed
+        background: "transparent",
+      },
       "& + .MuiSwitch-track": {
-        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
         opacity: 1,
-        border: 0,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
       },
-      "&.Mui-disabled + .MuiSwitch-track": {
-        opacity: 0.5,
-      },
-    },
-    "&.Mui-focusVisible .MuiSwitch-thumb": {
-      color: "#33cf4d",
-      border: "6px solid #fff",
-    },
-    "&.Mui-disabled .MuiSwitch-thumb": {
-      color:
-        theme.palette.mode === "light"
-          ? theme.palette.grey[100]
-          : theme.palette.grey[600],
-    },
-    "&.Mui-disabled + .MuiSwitch-track": {
-      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
     },
   },
   "& .MuiSwitch-thumb": {
-    boxSizing: "border-box",
-    width: 22,
-    height: 22,
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 150,
+    height: 70,
+    borderRadius: 10,
+    "&:before": {
+      content: '"2"', // Change content to "2"
+      position: "absolute",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      color: "#fff",
+      fontSize: "24px", // Adjust font size as needed
+      background: "transparent",
+    },
   },
   "& .MuiSwitch-track": {
-    borderRadius: 26 / 2,
-    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
     opacity: 1,
-    transition: theme.transitions.create(["background-color"], {
-      duration: 500,
-    }),
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
   },
 }));
+
 export default function CustomizedSwitches() {
   return (
     <FormGroup>
       <FormControlLabel
-        control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
-        label="iOS style"
+        control={<MaterialUISwitch sx={{ m: "auto" }} defaultChecked />}
+        label=""
       />
     </FormGroup>
   );
