f20ab618527997715a697fe7fff878148a965783
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2023-09-10 22:42:14 +0530

    final

--- src/components/I+T_form.js
+++ src/components/I+T_form.js
@@ -72,211 +72,217 @@ export default function EnrollmentForm(props) {
   };
 
   return (
-    <div style={{ position: "relative" }}>
-      <div style={textStyle}>
-        <h5><b>Are you prepared to begin the enrollment process?</b></h5>
-        <p>Please complete the following form and submit the required   </p><p> enrollment
-        fees to finalize your enrollment.</p>
-      </div>
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div style={{ position: "relative" }}>
+        <div style={textStyle}>
+          <h5>
+            <b>Are you prepared to begin the enrollment process?</b>
+          </h5>
+          <p>Please complete the following form and submit the required </p>
+          <p> enrollment fees to finalize your enrollment.</p>
+        </div>
 
-    <img src={purp} alt="Overlay Image" style={overlayImageStyle} />
+        <img src={purp} alt="Overlay Image" style={overlayImageStyle} />
 
-
-      <Grid item xs={6} sx={{ boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{
-            "& > :not(style)": { m: 1 },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            value={Name}
-            onChange={(e) => setName(e.target.value)}
-            fullWidth
-            label="Name"
-            id="name"
+        <Grid item xs={6} sx={{ boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
             sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "60%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
+              "& > :not(style)": { m: 1 },
             }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-
-          <TextField
-            value={Col}
-            onChange={(e) => setCol(e.target.value)}
-            fullWidth
-            label="College"
-            sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "60%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
-            }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-          <TextField
-            value={Deg}
-            onChange={(e) => setDeg(e.target.value)}
-            fullWidth
-            label="Degree"
-            sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "60%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
-            }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-          <TextField
-            value={CurrSem}
-            onChange={(e) => setCurrSem(e.target.value)}
-            fullWidth
-            label="Semester"
-            type="number"
-            inputProps={{
-              min: 1,
-              max: 10,
-            }}
-            sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "30%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
-            }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-
-          <TextField
-            value={Email}
-            onChange={(e) => setEmail(e.target.value)}
-            fullWidth
-            label="Email ID"
-            sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "60%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
-            }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-          <TextField
-            value={Phone}
-            onChange={(e) => setPhone(e.target.value)}
-            fullWidth
-            label="Phone Number"
-            sx={{
-              "& .MuiInputLabel-root": { color: "white" },
-              "& .MuiOutlinedInput-root": {
-                "& > fieldset": { borderColor: "white" },
-                width: "60%",
-              },
-              "& .MuiOutlinedInput-root:hover": {
-                "& > fieldset": {
-                  borderColor: "#2468ec",
-                },
-              },
-            }}
-            InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
-            InputProps={{
-              style: { color: "white" },
-            }}
-          />
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={["DatePicker"]}>
-              <DatePicker
-                value={Datee}
-                onChange={(date) => setDatee(date)}
-                color="white"
-                label="Basic date picker"
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
-                    },
-                  },
-                  "& .MuiFormLabel-root": {
-                    color: "white",
-                  },
-                  "& .MuiInputLabel-outlined.Mui-focused": {
-                    color: "white",
-                  },
-                  "& .MuiSvgIcon-root": {
-                    color: "white",
-                  },
-                  "& .MuiInputBase-input": {
-                    color: "white",
-                  },
-                }}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-          <button
-            disabled={!isFormValid()}
-            onClick={handleSubmit}
-            style={{
-              backgroundColor: !isFormValid() ? "#EBEBE4" : "red",
-              borderRadius: "50px",
-              border: "2px solid white",
-              color: "white",
-              fontSize: "12px",
-              padding: "10px 20px",
-            }}
+            noValidate
+            autoComplete="off"
           >
-            <Typography variant="h5">Submit</Typography>
-          </button>
-        </Box>
-      </Grid>
+            <TextField
+              value={Name}
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              label="Name"
+              id="name"
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "60%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+
+            <TextField
+              value={Col}
+              onChange={(e) => setCol(e.target.value)}
+              fullWidth
+              label="College"
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "60%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+            <TextField
+              value={Deg}
+              onChange={(e) => setDeg(e.target.value)}
+              fullWidth
+              label="Degree"
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "60%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+            <TextField
+              value={CurrSem}
+              onChange={(e) => setCurrSem(e.target.value)}
+              fullWidth
+              label="Semester"
+              type="number"
+              inputProps={{
+                min: 1,
+                max: 10,
+              }}
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "30%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+
+            <TextField
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              label="Email ID"
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "60%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+            <TextField
+              value={Phone}
+              onChange={(e) => setPhone(e.target.value)}
+              fullWidth
+              label="Phone Number"
+              sx={{
+                "& .MuiInputLabel-root": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": { borderColor: "white" },
+                  width: "60%",
+                },
+                "& .MuiOutlinedInput-root:hover": {
+                  "& > fieldset": {
+                    borderColor: "#2468ec",
+                  },
+                },
+              }}
+              InputLabelProps={{ shrink: true, sx: { color: "#fff" } }}
+              InputProps={{
+                style: { color: "white" },
+              }}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  value={Datee}
+                  onChange={(date) => setDatee(date)}
+                  color="white"
+                  label="Basic date picker"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "white",
+                      },
+                    },
+                    "& .MuiFormLabel-root": {
+                      color: "white",
+                    },
+                    "& .MuiInputLabel-outlined.Mui-focused": {
+                      color: "white",
+                    },
+                    "& .MuiSvgIcon-root": {
+                      color: "white",
+                    },
+                    "& .MuiInputBase-input": {
+                      color: "white",
+                    },
+                  }}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+            <button
+              disabled={!isFormValid()}
+              onClick={handleSubmit}
+              style={{
+                backgroundColor: !isFormValid() ? "#EBEBE4" : "red",
+                borderRadius: "50px",
+                border: "2px solid white",
+                color: "white",
+                fontSize: "12px",
+                padding: "10px 20px",
+              }}
+            >
+              <Typography variant="h5">Submit</Typography>
+            </button>
+          </Box>
+        </Grid>
+      </div>
     </div>
   );
 }
