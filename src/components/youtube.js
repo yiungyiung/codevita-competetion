2d010e4df3e074180327eb547a60ca1fe75f6793
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2023-09-10 17:31:13 +0530

    youtube

---
+++ src/components/youtube.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const VideoLinks = () => {
+  const youtubeVideoLink1 = 'https://youtu.be/h4X8rKDSpjo'; 
+  const youtubeVideoLink2 = 'https://youtu.be/J09qee7fYfk'; 
+
+  const videoContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr', 
+    gap: '16px', 
+  };
+
+  const pageTitleStyle = {
+    color: '#2468ec',
+    textAlign: 'center', 
+    fontSize: '45px'
+  };
+
+  return (
+    <div>
+      <h1 style={pageTitleStyle}>Checkout What Our Ex Interns Have To Say</h1>
+      <div style={videoContainerStyle}>
+        <iframe
+          width="560"
+          height="315"
+          src={youtubeVideoLink1.replace('youtu.be/', 'www.youtube.com/embed/')}
+          title="Embedded YouTube Video 1"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src={youtubeVideoLink2.replace('youtu.be/', 'www.youtube.com/embed/')}
+          title="Embedded YouTube Video 2"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <br/><br/>
+    </div>
+  );
+};
+
+export default VideoLinks;
